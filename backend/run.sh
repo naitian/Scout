a0e956f78949cd619e45eaaac5c110ea938656ab
Naitian Zhou <nzhou2011@gmail.com>
2018-01-27 21:33:14 -0500

    Remove stupid stuff

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PRODUCTION="TRUE"
 cd /web/projects/scope-backend/scope/backend
-/web/projects/scope-backend/scope/env/bin/gunicorn scope_server.wsgi -b 127.0.0.1:$PORT -w=15
+/web/projects/scope-backend/scope/env/bin/gunicorn wsgi -b 127.0.0.1:$PORT -w=15

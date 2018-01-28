4873bd8cc862a265e2882365ba358f499168cc59
Naitian Zhou <nzhou2011@gmail.com>
2018-01-27 21:19:58 -0500

    Add Server

---
+++ backend/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PRODUCTION="TRUE"
+cd /web/projects/scope-backend/scope/backend
+/web/projects/scope-backend/scope/env/bin/gunicorn scope_server.wsgi -b 127.0.0.1:$PORT -w=15
---
+++ backend/scope_server/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from .indexer import index_video
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/process")
+def process():
+    if request.method == 'POST':
+        index_video(request.form['url'])

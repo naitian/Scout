e5468e8251c1755c6ea0370672de6fefcd0723c0
Naitian Zhou <nzhou2011@gmail.com>
2018-01-27 22:26:37 -0500

    Don't Process if in DB

--- backend/server.py
+++ backend/server.py
@@ -2,6 +2,7 @@ from threading import Thread
 
 from flask import Flask, request
 from indexer import index_video
+import boto3
 
 app = Flask(__name__)
 
@@ -14,6 +15,17 @@ def hello():
 @app.route("/process")
 def process():
     if request.method == 'POST':
+        client = boto3.client("dynamodb")
+        res = client.get_item(
+            TableName='index',
+            Key={
+                'url': {
+                    'S': request.form['url']
+                }
+            }
+        )
+        if 'Item' in res:
+            return "Already Exists"
         background_thread = Thread(target=index_video, args=(request.form['url'],))
         background_thread.start()
         return "Processing"
